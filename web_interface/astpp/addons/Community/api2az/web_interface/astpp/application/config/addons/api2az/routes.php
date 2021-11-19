f7986cc867760c246e3aa05f2811cfd0971dec03
Gonzalo Moreno <gonzalo.moreno@polaris.es>
2021-11-19 14:08:47 +0100

    Añadimos el API como un addon

---
+++ web_interface/astpp/addons/Community/api2az/web_interface/astpp/application/config/addons/api2az/routes.php
@@ -0,0 +1,6 @@
+<?php
+$route ['api2az/getbalance/(:any)'] = "api2az/getbalance/$1";
+$route ['api2az/getnextfreeaccount/(:any)/(:any)/(:any)'] = "api2az/getnextfreeaccount/$1/$2/$3";
+$route ['api2az/getcalls/(:any)/(:any)'] = "api2az/getcalls/$1/$2";
+$route ['api2az/getcountries'] = "api2az/getcountries";
+?>
